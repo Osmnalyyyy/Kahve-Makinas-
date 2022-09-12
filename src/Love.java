903fc0b753c012892b4dfba4ab702a6e83436e42
Osmnalyyyy <108487432+Osmnalyyyy@users.noreply.github.com>
2022-09-12 15:31:21 +0300

    22.09.12

---
+++ src/Love.java
@@ -0,0 +1,16 @@
+public class Love {
+    public static void main(String[] args) {
+
+        String sevgi = "Seni cok seviyorum SEVGI";
+        String ekle = "o";
+        int index = sevgi.indexOf("o");
+
+        System.out.println(sevgi);
+
+        for (int i = 0; i < 1000; i++) {
+
+            sevgi = sevgi.substring(0, index) + ekle + sevgi.substring(index);
+            System.out.println(sevgi);
+        }
+    }
+}
